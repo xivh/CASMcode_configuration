3926d2b8a685f39349bdae7a230676e4b60c736f
bpuchala <bpuchala@users.noreply.github.com>
2021-11-23 12:38:25 -0500

    add ising SupercellSymOp test

--- tests/unit/configuration/SupercellSymOp_test.cpp
+++ tests/unit/configuration/SupercellSymOp_test.cpp
@@ -81,8 +81,6 @@ TEST_F(SupercellSymOpFCCTernaryGLStrainDispTest, Test1) {
   auto begin = config::SupercellSymOp::begin(supercell);
   auto end = config::SupercellSymOp::end(supercell);
 
-  // ConfigDoFValues copy_apply(SupercellSymOp const &op,
-  //                            ConfigDoFValues dof_values);
   dof_values.occupation(0) = 1;
   dof_values.local_dof_values.at("disp")(0, 0) = 1.0;
   dof_values.global_dof_values.at("GLstrain")(0) = 0.01;
@@ -116,4 +114,38 @@ TEST_F(SupercellSymOpFCCTernaryGLStrainDispTest, Test1) {
     GLstrain_count += transformed_dof_values.global_dof_values.at("GLstrain");
   }
   EXPECT_TRUE(almost_equal(GLstrain_count, Eigen::VectorXd::Zero(6)));
+}
+
+class SupercellSymOpSimpleCubicIsingTest : public testing::Test {
+ protected:
+  SupercellSymOpSimpleCubicIsingTest() {
+    auto prim =
+        std::make_shared<config::Prim const>(test::SimpleCubic_ising_prim());
+    Eigen::Matrix3l T = Eigen::Matrix3l::Identity() * 2;
+    supercell = std::make_shared<config::Supercell const>(prim, T);
+  }
+
+  std::shared_ptr<config::Supercell const> supercell;
+};
+
+TEST_F(SupercellSymOpSimpleCubicIsingTest, Test1) {
+  config::Configuration configuration(supercell);
+  clexulator::ConfigDoFValues dof_values = configuration.dof_values;
+  Index size = dof_values.occupation.size();
+  EXPECT_TRUE(almost_equal(dof_values.occupation, Eigen::VectorXi::Zero(size)));
+
+  auto begin = config::SupercellSymOp::begin(supercell);
+  auto end = config::SupercellSymOp::end(supercell);
+
+  dof_values.occupation(0) = 1;
+  Eigen::VectorXi occ_count = Eigen::VectorXi::Zero(size);
+
+  for (auto it = begin; it != end; ++it) {
+    clexulator::ConfigDoFValues transformed_dof_values =
+        copy_apply(*it, dof_values);
+    occ_count += transformed_dof_values.occupation;
+  }
+  // Expect 48*8 ops -> 7x down, 1x up; 48*8 ops -> 7x up, 1x down
+  EXPECT_TRUE(almost_equal(occ_count,
+                           Eigen::VectorXi::Constant(size, 1 * 48 + 7 * 48)));
 }
